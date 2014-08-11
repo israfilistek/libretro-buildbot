de09d005b8f3b8886a63e8b6380984d46952f9bd
l3iggs <john.f.grub@gmail.com>
2014-08-10 19:38:33 -0700

    add android skeleton

--- libretro-linux-buildimage/lr-build.sh
+++ libretro-linux-buildimage/lr-build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-linux_x86_64()
+updateCode()
 {
-  echo Building for Linux...
-  mkdir -p /output/linux/x86_64/cores
-  mkdir -p /nightly/linux/x86_64
+  echo Updating code...
   repo sync
   repo forall -c git submodule update --init
+}
+
+linux_x86_64()
+{
+  echo Building for Linux x86_64...
+  rm -rf /output/linux/x86_64/
+  mkdir -p /output/linux/x86_64/cores
+  mkdir -p /nightly/linux/x86_64
   cd libretro-super
   ./retroarch-build.sh
   ./libretro-build.sh
@@ -16,17 +22,40 @@ linux_x86_64()
   7za a -r /nightly/linux/x86_64/$(date +"%Y-%m-%d_%T")_retroarch-linux_x86_64.7z /output/linux/x86_64/*
 }
 
-
-android()
+android_armeabi-v7a()
 {
-  echo Building for Android...
+  echo Building for Android armeabi-v7a...
+  mkdir -p /nightly/android/armeabi-v7a
+  rm -rf /output/android/armeabi-v7a/
+  rm -rf /root/libretro-super/retroarch/android/phoenix/assets
+  mkdir -p /output/android/assets/armeabi-v7a/assets/cores
+  mkdir -p /output/android/assets/armeabi-v7a/assets/overlays
+  
+  #build the cores
+  cd libretrosuper
+  #TODO: ./libretro-build-android-mk.sh #it would be nice to run this through ccache too if possible
+  
+  #build retroarch
+  cd retroarch/android/phoenix
+  mkdir -p assets/cores
+  mkdir assets/overlays
   echo TODO
+  #cp /root/libretro-super/dist/android/armeabi-v7a/* asets/cores
+  cp /root/libretro-super/dist/info/* asets/cores
+  cp /root/libretro-super/libretro-overlays/* asets/overlays
+  #ant clean
+  #ant release
+  #TODO: sign the apk here
+  
+  #cp bin/release.apk /nightly/android/armeabi-v7a/$(date +"%Y-%m-%d_%T")_android-armeabi-v7a.apk
 }
 
 
 if [ $1 ]; then
-   $1
+  updateCode
+  $1
 else
+  updateCode
   linux_x86_64
-  android
+  android_armeabi-v7a
 fi
