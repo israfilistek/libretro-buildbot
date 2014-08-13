d27dbf61473dda07cee39ea243cbcdedd3dccc19
l3iggs <john.f.grub@gmail.com>
2014-08-13 11:05:11 -0700

    update build sctript
    android does not build with NOCLEAN by default, script does nothing without a target

--- nightly-build.sh
+++ nightly-build.sh
@@ -3,6 +3,7 @@
 updateCode()
 {
   echo Updating code...
+  cd /root/
   repo sync
   repo forall -c git submodule update --init
 }
@@ -14,12 +15,17 @@ linux_x86_64()
   rm -rf /root/libretro-super/dist/unix*
   mkdir -p /output/linux/x86_64/cores
   mkdir -p /nightly/linux/x86_64
-  cd libretro-super
+  
+  # build cores
+  cd /root/libretro-super
   ./retroarch-build.sh
-  #./libretro-build.sh
   ./libretro-install.sh /output/linux/x86_64/cores
+  
+  # build frontend
+  #./libretro-build.sh
   #cd retroarch/
   #make DESTDIR=/output/linux/x86_64 install
+  
   7za a -r /nightly/linux/x86_64/$(date +"%Y-%m-%d_%T")_retroarch-linux_x86_64.7z /output/linux/x86_64/*
 }
 
@@ -27,15 +33,15 @@ android_armeabi-v7a()
 {
   echo Building for Android armeabi-v7a...
   rm -rf /output/android/armeabi-v7a/
-  rm -rf /root/libretro-super/dist/android
+  rm -rf /root/libretro-super/dist/android*
   mkdir -p /output/android/armeabi-v7a/cores 
   mkdir -p /nightly/android/armeabi-v7a
 
   # build cores
   cd /root/libretro-super/
-  NOCLEAN=1 ./libretro-build-android-mk.sh #TODO: remove NOCLEAN when ccache is working for android builds
+  ./libretro-build-android-mk.sh
   
-  # build android frontend
+  # build frontend
   cd /root/libretro-super/retroarch/android/phoenix
   android update project --path .
   echo "ndk.dir=/root/android-tools/android-ndk" >> local.properties
@@ -58,8 +64,4 @@ android_armeabi-v7a()
 if [ $1 ]; then
   updateCode
   $1
-else
-  updateCode
-  linux_x86_64
-  android_armeabi-v7a
 fi
