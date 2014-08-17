76c097d5d7ca88a473a5237a285f0bd1a121f645
l3iggs <john.f.grub@gmail.com>
2014-08-17 09:53:38 -0700

    change output to staging

--- cron-linux-core-build.sh
+++ cron-linux-core-build.sh
@@ -8,13 +8,13 @@ docker pull l3iggs/core-builder
 # run the build
 docker run --cpuset="0,1,2" l3iggs/core-builder
 
-rm -rf /home/buildbot/output
-docker cp $(docker ps -l -q):/output /home/buildbot/
-mkdir -p /home/buildbot/output/linux/build-logs/
-docker logs $(docker ps -l -q) > /home/buildbot/output/linux/build-logs/core-build.log 2>&1
+rm -rf /home/buildbot/staging
+docker cp $(docker ps -l -q):/staging /home/buildbot/
+mkdir -p /home/buildbot/staging/linux/build-logs/
+docker logs $(docker ps -l -q) > /home/buildbot/staging/linux/build-logs/core-build.log 2>&1
 
-rm `find /home/buildbot/output/ -name *.info`
-ALL_CORES=`find /home/buildbot/output/ -name *.so`
+rm `find /home/buildbot/staging/ -name *.info`
+ALL_CORES=`find /home/buildbot/staging/ -name *.so`
 for c in $ALL_CORES
 do
   PARENT=`dirname $c`
@@ -24,7 +24,7 @@ do
   mv $c ${CORE_FOLDER}
 done
 
-ALL_FILES=`find /home/buildbot/output/ -type f`
+ALL_FILES=`find /home/buildbot/staging/ -type f`
 for f in $ALL_FILES
 do
   PARENT=`dirname $f`
@@ -33,4 +33,4 @@ do
 done
 
 mkdir -p /home/buildbot/www/nightly/
-cp -r  /home/buildbot/output/* /home/buildbot/www/nightly/
+cp -r  /home/buildbot/staging/* /home/buildbot/www/nightly/
