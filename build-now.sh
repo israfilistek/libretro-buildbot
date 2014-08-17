39457ace1172d5d90e93db77abe18577ecee68a7
l3iggs <john.f.grub@gmail.com>
2014-08-16 17:54:44 -0700

    Update and rename nightly-build.sh to build-now.sh

--- build-now.sh
+++ build-now.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # grabs the latest code for all of libretro
 update_code()
@@ -12,48 +12,47 @@ update_code()
 # builds the front end
 linux_retroarch()
 {
-  rm -rf /output/linux/
-  mkdir -p /output/linux/
-  mkdir -p /nightly/linux/
-  
-  
+  echo Building RetroArch ...
   # build frontend
   cd /root/libretro-super
   ./retroarch-build.sh
+  
+  rm -rf /output/linux/RetroArch/*
+  mkdir -p /output/linux/RetroArch/files
   cd /root/libretro-super/retroarch/
-  make DESTDIR=/output/linux/ install
-  7za a -r /nightly/linux/$(date +"%Y-%m-%d_%T")_retroarch-linux.7z /output/linux/*
+  make DESTDIR=/output/linux/RetroArch/files install
+  
+  7za a -r /output/linux/RetroArch.7z /output/linux/RetroArch/files/*
 }
 
 # builds all the cores
 linux_cores()
 {
-  echo Building cores ...
-  rm -rf /output/linux/
-  rm -rf /root/libretro-super/dist/unix*
-  mkdir -p /output/linux/cores
-  mkdir -p /nightly/linux/
-  
+  echo Building cores...
   # build cores
+  rm -rf /root/libretro-super/dist/unix*
   cd /root/libretro-super
   ./libretro-build.sh
-  ./libretro-install.sh /output/linux/cores
   
-  7za a -r /nightly/linux/$(date +"%Y-%m-%d_%T")_libretro-cores-linux.7z /output/linux/*
+  rm -rf /output/linux/cores/*
+  mkdir -p /output/linux/cores/files
+  cd /root/libretro-super
+  ./libretro-install.sh /output/linux/cores/files
+  
+  
+  7za a -r /output/linux/cores.7z /output/linux/cores/files/*
 }
 
 # builds the android frontend and cores and packages into an apk
 android_all()
 {
   echo Building for Android ...
-  rm -rf /output/android/
-  rm -rf /root/libretro-super/dist/android*
-  mkdir -p /output/android/cores 
-  mkdir -p /nightly/android
-
   # build cores
+  rm -rf /root/libretro-super/dist/android*
   cd /root/libretro-super/
   ./libretro-build-android-mk.sh
+  
+  mkdir -p /nightly/android
   
   # build frontend
   cd /root/libretro-super/retroarch/android/phoenix
@@ -81,8 +80,13 @@ android_all()
   # sign the apk
   jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -storepass libretro -keystore /root/android-tools/my-release-key.keystore /root/libretro-super/retroarch/android/phoenix/bin/retroarch-release-unsigned.apk retroarch
   
+  rm -rf /output/android/*
+  mkdir -p /output/android/cores
+  cp /root/libretro-super/retroarch/android/phoenix/assets/cores/* /output/android/cores/
+  7za a -r /output/android/cores.7z /output/android/cores/*
+  
   # zipalign
-  `find /root/android-tools/android-sdk-linux/ -name zipalign` -v 4 /root/libretro-super/retroarch/android/phoenix/bin/retroarch-release-unsigned.apk /nightly/android/$(date +"%Y-%m-%d_%T")_android.apk
+  `find /root/android-tools/android-sdk-linux/ -name zipalign` -v 4 /root/libretro-super/retroarch/android/phoenix/bin/retroarch-release-unsigned.apk /output/android/RetroArch.apk
 }
 
 
