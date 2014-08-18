d4f38571e12a9bf63898fccb1ae0ccda9e16c376
l3iggs <john.f.grub@gmail.com>
2014-08-18 09:02:35 -0700

    change .log to .txt for browser rendering

--- cron-android-build.sh
+++ cron-android-build.sh
@@ -13,7 +13,7 @@ docker run --cpuset="0,1,2" libretro/android-builder
 rm -rf /home/buildbot/staging
 docker cp $(docker ps -l -q):/staging /home/buildbot/
 mkdir -p /home/buildbot/staging/android/build-logs/
-docker logs $(docker ps -l -q) > /home/buildbot/staging/android/build-logs/build.log 2>&1
+docker logs $(docker ps -l -q) > /home/buildbot/staging/android/build-logs/build.txt 2>&1
 
 ALL_CORES=`find /home/buildbot/staging/ -name *.so`
 for c in $ALL_CORES
