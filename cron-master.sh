abede1f0d89980bd96e33aae18aa09e84fe221c3
l3iggs <john.f.grub@gmail.com>
2014-08-19 21:05:08 -0700

    Update cron-master.sh

--- cron-master.sh
+++ cron-master.sh
@@ -8,7 +8,7 @@
 # has write access to that directory and can use the docker command
 
 # these should be names of scripts in https://github.com/libretro/libretro-buildbot/tree/master
-declare -a BUILD_SCRIPTS=("cron-android-build.sh" "cron-linux-core-build.sh")
+declare -a BUILD_SCRIPTS=("cron-android-build.sh" "cron-linux-core-build.sh" "cron-wondows-core-build.sh")
 
 # for each script, make sure it's up to date from the repo and then run it
 for s in "${BUILD_SCRIPTS[@]}"
