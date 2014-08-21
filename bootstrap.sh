73da50bbd8f11775f8a54b0e3ab7ea704170b337
l3iggs <john.f.grub@gmail.com>
2014-08-21 10:34:19 -0700

    allow local manifest to be fetched during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -e
 
-echo "Updating code..."
+if [ -z "MANIFEST_URL" ]; then
+  echo "Updating code..."
+else
+  echo "Updating code using custom repositories..."
+  mkdir -p /root/.repo/local_manifests
+  curl MANIFEST_URL > /root/.repo/local_manifests/my_manifest.xml
+fi
 cd /root/
 repo sync
 repo forall -c git submodule update --init
 
-cp /root/libretro-super/libretro-buildbot/build-now.sh /bin/build-now.sh
-chmod a+x /bin/build-now.sh
-
-build-now.sh $1
+if [ $1 ]; then
+  echo "Building $1..."
+  cp /root/libretro-super/libretro-buildbot/build-now.sh /bin/build-now.sh
+  chmod a+x /bin/build-now.sh
+  build-now.sh $1
+fi
