9b5182711e2717a06704da4b15a5aff523bcac5c
l3iggs <john.f.grub@gmail.com>
2014-08-21 10:07:40 -0700

    need run permissions for build-now.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,7 @@ cd /root/
 repo sync
 repo forall -c git submodule update --init
 
-/root/libretro-super/libretro-buildbot/build-now.sh $1
+cp /root/libretro-super/libretro-buildbot/build-now.sh /bin/build-now.sh
+chmod a+x /bin/build-now.sh
+
+build-now.sh $1
