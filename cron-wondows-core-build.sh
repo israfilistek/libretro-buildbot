11948d628d7d40433eae9f388a60ed054496eb86
l3iggs <john.f.grub@gmail.com>
2014-08-21 22:27:11 -0700

    change log name

--- cron-wondows-core-build.sh
+++ cron-wondows-core-build.sh
@@ -3,6 +3,7 @@
 # readies the files it generates for http consumption
 
 TODAY_IS=`date +"%Y-%m-%d"`
+LOG_NAME=core_build.txt
 
 # ensure the image is up to date
 docker pull libretro/win-core-builder
@@ -13,9 +14,9 @@ docker run --cpuset="0,1,2" libretro/win-core-builder
 rm -rf /home/buildbot/staging
 docker cp $(docker ps -l -q):/staging /home/buildbot/
 mkdir -p /home/buildbot/staging/windows/build-logs/
-docker logs $(docker ps -l -q) > /home/buildbot/staging/windows/build-logs/build.txt 2>&1
-cat -n /home/buildbot/staging/windows/build-logs/build.txt > /home/buildbot/staging/windows/build-logs/build_num.txt
-mv /home/buildbot/staging/windows/build-logs/build_num.txt /home/buildbot/staging/windows/build-logs/build.txt
+docker logs $(docker ps -l -q) > /home/buildbot/staging/windows/build-logs/${LOG_NAME} 2>&1
+cat -n /home/buildbot/staging/windows/build-logs/${LOG_NAME} > /home/buildbot/staging/windows/build-logs/${LOG_NAME}_num
+mv /home/buildbot/staging/windows/build-logs/${LOG_NAME}_num /home/buildbot/staging/windows/build-logs/${LOG_NAME}
 
 rm `find /home/buildbot/staging/ -name *.info`
 ALL_CORES=`find /home/buildbot/staging/ -name *.dll`
