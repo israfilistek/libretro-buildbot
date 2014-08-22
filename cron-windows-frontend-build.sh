630dbcbcc2afc14e0dce7cca87fb1d92678251eb
l3iggs <john.f.grub@gmail.com>
2014-08-22 11:20:18 -0700

    Create cron-windows-frontend-build.sh

---
+++ cron-windows-frontend-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+# this script lives in the webserver and triggers the RetroArch build for Fedora 20
+# and readies the files it generates for http consumption
+
+TODAY_IS=`date +"%Y-%m-%d"`
+LOG_NAME=frontend_windows.txt
+
+# ensure the image is up to date
+docker pull libretro/retroarch-win-builder
+
+# run the build
+docker run --cpuset="0,1,2" libretro/retroarch-win-builder
+
+rm -rf /home/buildbot/staging
+docker cp $(docker ps -l -q):/staging /home/buildbot/
+
+mkdir -p /home/buildbot/staging/linux/build-logs/
+docker logs $(docker ps -l -q) > /home/buildbot/staging/linux/build-logs/${LOG_NAME} 2>&1
+cat -n /home/buildbot/staging/linux/build-logs/${LOG_NAME} > /home/buildbot/staging/linux/build-logs/${LOG_NAME}_num.txt
+mv /home/buildbot/staging/linux/build-logs/${LOG_NAME}_num.txt /home/buildbot/staging/linux/build-logs/${LOG_NAME}
+
+ALL_FILES=`find /home/buildbot/staging/ -type f`
+for f in $ALL_FILES
+do
+  PARENT=`dirname $f`
+  FILE_NAME=`basename $f`
+  mv $f ${PARENT}/${TODAY_IS}_${FILE_NAME}
+  ln -sf ./${TODAY_IS}_${FILE_NAME} ${PARENT}/latest_${FILE_NAME}
+done
+
+mkdir -p /home/buildbot/www/nightly/
+cp -r  /home/buildbot/staging/* /home/buildbot/www/nightly/
