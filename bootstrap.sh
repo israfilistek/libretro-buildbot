8746684d5981e783d6c2a4c84d3ba010ce23a089
l3iggs <john.f.grub@gmail.com>
2014-08-23 09:52:57 -0700

    clean up repos after build

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,5 +16,8 @@ chmod a+x /bin/build-now.sh
 
 if [ $1 ]; then
   echo "Building $1 $2..."
-  build-now.sh $1 $2
+  build-now.sh $1 $2 || echo "Non-zero returned from build-now.sh
+
+  # clean up cruft from all builds, this prevents possible future code update issues
+  cd /root && repo for all -c 'git clean -fd'
 fi
