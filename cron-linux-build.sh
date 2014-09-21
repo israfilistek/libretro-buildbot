824b24ba180225f6a2f77f4401308a6696c4e28b
l3iggs <john.f.grub@gmail.com>
2014-09-21 10:03:55 -0700

    enable building for multiple linux distros

--- cron-linux-build.sh
+++ cron-linux-build.sh
@@ -3,40 +3,45 @@
 # readies the files it generates for http consumption
 
 TODAY_IS=`date +"%Y-%m-%d"`
-LOG_NAME=cores
+LOG_NAME=build
 
-# ensure the image is up to date
-docker pull libretro/build-linux-arch
+#these strings must match the names of the repos in registry.hub.docker.com (prefixed with "libretro/build-linux-")
+declare -a DISTROS=("arch_linux" "ubuntu_14.04" "fedora_20")
 
-# run the build
-docker run --cpuset="0,1,2" libretro/build-linux-arch
+for DISTRO in "${DISTROS[@]}"
+  do
+  # ensure the image is up to date
+  docker pull libretro/build-linux-${DISTRO}
 
-rm -rf /home/buildbot/staging
-docker cp $(docker ps -l -q):/staging /home/buildbot/
-mkdir -p /home/buildbot/staging/linux/build-logs/
-docker logs $(docker ps -l -q) > /home/buildbot/staging/linux/build-logs/${LOG_NAME}.txt 2>&1
-cat -n /home/buildbot/staging/linux/build-logs/${LOG_NAME}.txt > /home/buildbot/staging/linux/build-logs/${LOG_NAME}_num.txt
-mv /home/buildbot/staging/linux/build-logs/${LOG_NAME}_num.txt /home/buildbot/staging/linux/build-logs/${LOG_NAME}.txt
+  # run the build
+  docker run --cpuset="0,1,2" libretro/build-linux-${DISTRO}
 
-rm `find /home/buildbot/staging/ -name *.info`
-ALL_CORES=`find /home/buildbot/staging/ -name *.so`
-for c in $ALL_CORES
-do
-  PARENT=`dirname $c`
-  CORE_NAME=`basename $c`
-  CORE_FOLDER=${PARENT}/${CORE_NAME%%.*}
-  mkdir ${CORE_FOLDER}
-  mv $c ${CORE_FOLDER}
+  rm -rf /home/buildbot/staging
+  docker cp $(docker ps -l -q):/staging /home/buildbot/
+  mkdir -p /home/buildbot/staging/linux/${DISTRO}/build-logs/
+  docker logs $(docker ps -l -q) > /home/buildbot/staging/linux/${DISTRO}/build-logs/${LOG_NAME}.txt 2>&1
+  cat -n /home/buildbot/staging/linux/${DISTRO}/build-logs/${LOG_NAME}.txt > /home/buildbot/staging/linux/${DISTRO}/build-logs/${LOG_NAME}_num.txt
+  mv /home/buildbot/staging/linux/${DISTRO}/build-logs/${LOG_NAME}_num.txt /home/buildbot/staging/linux/${DISTRO}/build-logs/${LOG_NAME}.txt
+
+  rm `find /home/buildbot/staging/ -name *.info`
+  ALL_CORES=`find /home/buildbot/staging/ -name *.so`
+  for c in $ALL_CORES
+  do
+    PARENT=`dirname $c`
+    CORE_NAME=`basename $c`
+    CORE_FOLDER=${PARENT}/${CORE_NAME%%.*}
+    mkdir ${CORE_FOLDER}
+    mv $c ${CORE_FOLDER}
+  done
+
+  ALL_FILES=`find /home/buildbot/staging/ -type f`
+  for f in $ALL_FILES
+  do
+    PARENT=`dirname $f`
+    FILE_NAME=`basename $f`
+    mv $f ${PARENT}/${TODAY_IS}_${FILE_NAME}
+    ln -sf ./${TODAY_IS}_${FILE_NAME} ${PARENT}/latest_${FILE_NAME}
+  done
 done
-
-ALL_FILES=`find /home/buildbot/staging/ -type f`
-for f in $ALL_FILES
-do
-  PARENT=`dirname $f`
-  FILE_NAME=`basename $f`
-  mv $f ${PARENT}/${TODAY_IS}_${FILE_NAME}
-  ln -sf ./${TODAY_IS}_${FILE_NAME} ${PARENT}/latest_${FILE_NAME}
-done
-
 mkdir -p /home/buildbot/www/nightly/
 cp -r  /home/buildbot/staging/* /home/buildbot/www/nightly/
